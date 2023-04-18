2b6b7a2264bdbaf524e9ad69f73d47649c673dd9
Sid <siddharth@habu.com>
2023-04-17 17:29:37 -0700

    Secure Key Release Demo

--- cvm-python-app-remoteattest/attestation_client.py
+++ cvm-python-app-remoteattest/attestation_client.py
@@ -22,18 +22,18 @@ if __name__ == "__main__":
 
         if TOKEN_FILE_PATH:
             # Split the JWT token into three parts
-            _, payload, _ = jwt_from_stdout.split('.')
+            _, payload, _ = jwt_from_stdout.split(".")
 
             # Decode the Base64Url encoded strings
             # decoded_header = base64.urlsafe_b64decode(header + '===').decode('utf-8')
-            decoded_payload = base64.urlsafe_b64decode(payload + '===').decode('utf-8')
+            decoded_payload = base64.urlsafe_b64decode(payload + "===").decode("utf-8")
 
-            with open(TOKEN_FILE_PATH + "/maa-raw.txt", 'w') as file:
+            with open(TOKEN_FILE_PATH + "/maa-raw.txt", "w") as file:
                 file.write(jwt_from_stdout)
 
-            with open(TOKEN_FILE_PATH + "/maa.json", 'w') as file:
+            with open(TOKEN_FILE_PATH + "/maa.json", "w") as file:
                 file.write(decoded_payload)
         else:
             raise FileNotFoundError("TOKEN_FILE_PATH environment variable not set.")
     else:
-        raise ValueError(f"MAA JWT was not formed properly: {jwt_from_stdout}")
+        raise ValueError(f"Error in fetching MAA token: {jwt_from_stdout}")
